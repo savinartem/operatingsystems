31fce6b84299a11d90218264aceaa7b6cfec4ba6
savinartem <74740204+savinartem@users.noreply.github.com>
2022-11-04 01:20:01 -0400

    Create shell.c

---
+++ shellProject/shell.c
@@ -0,0 +1,17 @@
+/*
+	Simple shell for testing group project
+
+*/
+
+#include <stdio.h>
+#include "shell.h"
+
+void print_prompt1(void)
+{
+    fprintf(stderr, "$ ");
+}
+
+void print_prompt2(void)
+{
+    fprintf(stderr, "> ");
+}
